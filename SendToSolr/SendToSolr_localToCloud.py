0d84343cc704fe4c1b1f2c71ad13a34c805991cc
Aadith Ramia <aadith.ramia@gmail.com>
2024-12-26 14:27:00 -0800

    SendToSolr_localToCloud - handling http 413 by subchunking

--- SendToSolr/SendToSolr_localToCloud.py
+++ SendToSolr/SendToSolr_localToCloud.py
@@ -17,15 +17,19 @@ from pyspark.sql import SparkSession, Row
 from pyspark.sql import functions as F
 
 
-
-#input_stream = "adl://bingads-platform-prod-vc1-c08.azuredatalakestore.net/local/users/shravanr/BPG/Brand/BrandData_Sample1000.csv"
-input_stream = r"C:\Users\shravanr\learning\spark\input_dir\BrandData_Raw_Sample1000.csv"
-batch_size = 500 # batch size for Spark partitioning
+BrandSchema = "SolrDocId string, BrandId long, BrandName string, BrandAliases string, Domains string, NumProducts int, SampleProductNames string, NumRetailers int, TopRetailers string, BPGVersionId string, Timestamp string"
+input_stream = r"C:\Users\shravanr\Downloads\BrandData_20241226125735.tsv.sample10k"
+batch_size = 1000 # batch size for Spark partitioning
 
 # Solr connection details
-SOLR_URL = 'http://51.8.239.64/solr/PCBrands'
+SOLR_URL = 'http://51.8.239.64/solr/Brands'
 USERNAME = 'igs'
 PASSWORD = 'igs345'
+
+print(f"Input Stream: {input_stream}")
+print(f"Batch Size: {batch_size}")
+print(f"Solr URL: {SOLR_URL}")
+
 
 def get_chunks(iterator, size):
         """Yield successive chunks from the iterator."""
@@ -37,6 +41,31 @@ def get_chunks(iterator, size):
                 chunk = []
         if chunk:
             yield chunk
+
+def send_chunk(chunk, solr):
+    isLargeBatch = False
+    try:
+        start_time = time.time()
+        solr.add(chunk)
+        end_time = time.time()
+        elapsed_time = end_time - start_time
+        print("Indexed {} documents in {:.2f} seconds.".format(len(chunk), elapsed_time))
+    except pysolr.SolrError as e:
+        if str(e).__contains__("HTTP 413"):
+            chunk_size = len(chunk)
+            if chunk_size == 1:                
+                print(f"Error indexing document: Request Entity Too Large (HTTP 413) : {chunk[0]}.")
+            else:
+                print(f"Error indexing batch of size {len(chunk)}: Request Entity Too Large (HTTP 413). Splitting chunk")
+                isLargeBatch = True
+    except Exception as e:
+        print(f"Error indexing batch: {e}")
+    if isLargeBatch:        
+        sub_chunk_size = int(len(chunk)/2)        
+        sub_chunks = [chunk[:sub_chunk_size] , chunk[sub_chunk_size:]]        
+        for sub_chunk in sub_chunks:
+            send_chunk(sub_chunk, solr)  
+
 
 def index_batch(batch):
     """Index a batch of documents to Solr with authentication."""
@@ -50,14 +79,7 @@ def index_batch(batch):
     solr = pysolr.Solr(SOLR_URL, always_commit=False, session=session)
     
     for chunk in get_chunks(batch, batch_size):
-        try:
-            start_time = time.time()
-            solr.add(chunk)
-            end_time = time.time()
-            elapsed_time = end_time - start_time
-            print("Indexed {} documents in {:.2f} seconds.".format(len(chunk), elapsed_time))
-        except Exception as e:
-            print(f"Error indexing batch: {e}")
+        send_chunk(chunk, solr)           
 
 
 
@@ -68,7 +90,7 @@ spark = SparkSession.builder.appName("SendToSolr").getOrCreate()
 df = spark.read.format("csv") \
     .option("delimiter", "\t") \
     .option("encoding", "UTF-8") \
-    .schema("BrandId long, BrandName string, BrandAliases string, Domains string, NumProducts int, SampleProductNames string, NumRetailers int, TopRetailers string")\
+    .schema(BrandSchema)\
     .option("header", "false") \
     .load(input_stream)
 
