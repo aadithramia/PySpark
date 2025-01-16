2b7e1b581fd069a149aa1a5f22a30a4a30ef526c
Aadith Ramia <aadith.ramia@gmail.com>
2025-01-15 16:51:12 -0800

    adding windowing aggregates

---
+++ SparkCore/Aggregates/WindowingAggregates.py
@@ -0,0 +1,25 @@
+import findspark
+findspark.init()
+from pyspark.sql import SparkSession
+from pyspark.sql.types import *
+from pyspark.sql.functions import *
+from pyspark.sql.window import Window
+
+spark = SparkSession.builder.appName("static_static_join")\
+	.master("local[3]") \
+	.config("spark.streaming.stopGracefullyOnShutdown", "true") \
+	.config("spark.sql.shuffle.partitions", 3) \
+	.getOrCreate()
+
+A = spark.read \
+    .option("sep", "\t") \
+    .option("header", "true") \
+    .csv(r"C:\Users\shravanr\learning\spark\pyspark\SparkCore\Aggregates\WindowingAggregatesData.tsv") 
+
+A.show()
+
+running_total_window = Window.partitionBy("Country") \
+						.orderBy("WeekNumber") \
+						.rowsBetween(Window.unboundedPreceding, Window.currentRow)
+
+A.withColumn("RunningTotal", sum("Sales").over(running_total_window)).show()
